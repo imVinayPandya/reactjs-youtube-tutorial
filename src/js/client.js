cb6ce0bc614c5db608b3e3c303169ae38d62160d
Vinay Pandya <005vinay@gmail.com>
2018-07-13 18:45:42 +0530

    Composing multiple ReactJs components

--- src/js/client.js
+++ src/js/client.js
@@ -1,24 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Layout extends React.Component {
-  
-  constructor() {
-    super();
-    this.name = "vinay pandya ";
-  }
-
-  getName(greating) {
-    return this.name+ greating;
-  }
-
-  render() {
-    return (
-      <h1>Hello {this.getName("how r u?")}</h1>
-    );
-  }
-}
+import Layout from "./components/Layout";
 
 const app = document.getElementById("app");
-
 ReactDOM.render(<Layout />, app);
