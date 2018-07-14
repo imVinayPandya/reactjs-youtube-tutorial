cd6518b6caea900b88be0b2322ca1f4d6365886d
Vinay Pandya <005vinay@gmail.com>
2018-07-14 06:14:42 -0700

    React router & Intro to Single page apps with ReactJs

--- 2-react-router/src/js/client.js
+++ 2-react-router/src/js/client.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
 import Layout from "./pages/Layout";
+import Featured from "./pages/Featured";
+import Archives from "./pages/Archives";
+import Settings from "./pages/Settings";
 
 const app = document.getElementById("app");
-ReactDOM.render(<Layout />, app);
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Layout} />
+      <Route path="/archives" component={Archives} />
+      <Route path="/settings" component={Settings} />
+    </Switch>
+  </BrowserRouter>,
+  app);
--- 2-react-router/src/js/pages/Layout.js
+++ 2-react-router/src/js/pages/Layout.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
+import { Link, Route } from "react-router-dom";
+
 export default class Layout extends React.Component {
+
+  navigate() {
+    this.props.history.push("/", null)
+  }
 
   render() {
     return (
-      <h1>KillerNews.net</h1>
+      <div>
+        <h1>KillerNews.net</h1>
+        <br />
+        <Link to="archives" class="btn btn-danger">archives</Link>
+        <Link to="settings">
+          <button class="btn btn-success">
+            settings
+          </button>
+        </Link>
+
+        <button onClick={this.navigate.bind(this)}>Home</button>
+      </div>
     );
   }
 }
